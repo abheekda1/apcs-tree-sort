00c4fbd81a40ecb05b7a0ef4c7a4825dd689c4ff
Abheek Dhawan <67982792+abheekda1@users.noreply.github.com>
2023-01-18 00:11:38 +0000

    build: add path prefix

---
+++ deck/gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  pathPrefix: 'apcs-tree-sort',
+  //plugins: [
+  //  {
+  //    resolve: 'gatsby-theme-mdx-deck',
+  //    options: {
+  //      cli: true,
+  //      contentPath: 'slides',
+  //    },
+  //  },
+  //  {
+  //    resolve: 'gatsby-plugin-compile-es6-packages',
+  //    options: {
+  //      modules: ['mdx-deck', 'gatsby-theme-mdx-deck', '@mdx-deck/themes'],
+  //    },
+  //  },
+  //],
+};

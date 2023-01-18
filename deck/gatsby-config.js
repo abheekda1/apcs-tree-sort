a74a05e6ee20ad6b4aeb08552857b6abc5141d2c
Abheek <abheekd@protonmail.com>
2023-01-18 00:55:44 +0000

    feat: use gatsby for proper gh pages

--- deck/gatsby-config.js
+++ deck/gatsby-config.js
@@ -1,18 +1,18 @@
 module.exports = {
   pathPrefix: 'apcs-tree-sort',
-  //plugins: [
-  //  {
-  //    resolve: 'gatsby-theme-mdx-deck',
-  //    options: {
-  //      cli: true,
-  //      contentPath: 'slides',
-  //    },
-  //  },
-  //  {
-  //    resolve: 'gatsby-plugin-compile-es6-packages',
-  //    options: {
-  //      modules: ['mdx-deck', 'gatsby-theme-mdx-deck', '@mdx-deck/themes'],
-  //    },
-  //  },
-  //],
+  plugins: [
+    {
+      resolve: 'gatsby-theme-mdx-deck',
+      options: {
+        cli: true,
+        contentPath: 'decks',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-compile-es6-packages',
+      options: {
+        modules: ['mdx-deck', 'gatsby-theme-mdx-deck', '@mdx-deck/themes', '@mdx-deck'],
+      },
+    },
+  ],
 };
